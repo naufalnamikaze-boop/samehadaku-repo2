c9b9433fdf484e9a91e8de8f945288350e7e8c13
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-07 05:33:06 +0700

    Update build.gradle.kts

--- samehadaku/build.gradle.kts
+++ samehadaku/build.gradle.kts
@@ -6,23 +6,10 @@ plugins {
 
 android {
     namespace = "com.example.samehadaku"
+
     compileSdk = 35
 
     defaultConfig {
         minSdk = 21
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-kotlin {
-    jvmToolchain(17)
-}
-
-dependencies {
-    implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
-implementation("org.jsoup:jsoup:1.18.3")
 }
