73d0283da35e161e71c6312ee2e3a0b7ae0087d3
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-07 10:33:13 +0700

    Update build.gradle.kts

--- samehadaku/build.gradle.kts
+++ samehadaku/build.gradle.kts
@@ -6,4 +6,18 @@ plugins {
 
 android {
     namespace = "com.example.samehadaku"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
