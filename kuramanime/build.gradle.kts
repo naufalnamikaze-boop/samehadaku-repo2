5dd01d76a3b021ba2cea432aab86e05f4cbf11c5
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-08 00:11:09 +0700

    Update build.gradle.kts

--- kuramanime/build.gradle.kts
+++ kuramanime/build.gradle.kts
@@ -31,5 +31,6 @@ dependencies {
     cloudstream("com.lagradost:cloudstream3:pre-release")
 
     implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
     implementation("org.jsoup:jsoup:1.18.3")
 }
