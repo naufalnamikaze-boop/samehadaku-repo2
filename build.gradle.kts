f19dc448e65b24e6515d56a4781a2e85ad1a7bd4
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-07 10:30:05 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,3 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    dependencies {
-        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-    }
-}
