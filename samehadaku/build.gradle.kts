0c5be5ccbbf871f8cddd4f4ef2775b43a9acdd82
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-07 10:52:56 +0700

    Update build.gradle.kts

--- samehadaku/build.gradle.kts
+++ samehadaku/build.gradle.kts
@@ -23,7 +23,12 @@ kotlin {
 }
 
 dependencies {
-    implementation("com.lagradost:cloudstream3:pre-release")
-    implementation("org.jsoup:jsoup:1.18.3")
+    val cloudstream by configurations
+    val implementation by configurations
+
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+
+    implementation(kotlin("stdlib"))
     implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
 }
