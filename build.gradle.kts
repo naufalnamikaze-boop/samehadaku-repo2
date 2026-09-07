a1f0154a808891ccc07ee25ca3a2893009928c69
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-07 09:54:26 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
 buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
@@ -11,63 +6,15 @@ buildscript {
     }
 }
 
-fun Project.cloudstream(
-    configuration: CloudstreamExtension.() -> Unit
-) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(
-    configuration: BaseExtension.() -> Unit
-) = extensions.getByName<BaseExtension>("android").configuration()
-
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
-    cloudstream {
-        setRepo(
-            System.getenv("GITHUB_REPOSITORY")
-                ?: "https://github.com/user/repo"
-        )
-    }
-
-    android {
-        namespace = "recloudstream"
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-        }
-
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_17)
-
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
-                )
-            }
-        }
-    }
 
     dependencies {
-        val implementation by configurations
-
-        implementation(
-            "com.github.recloudstream.cloudstream:library:-SNAPSHOT"
-        )
-
-        implementation(kotlin("stdlib"))
+        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
-        implementation(
-            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
-        )
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     }
 }
