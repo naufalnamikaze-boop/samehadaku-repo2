4df5ab67ff3ebc75df2caa3bb4b7e7ce5204169d
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-08 10:45:07 +0700

    Create DutamoviePlugin.kt

---
+++ Dutamovie/src/main/kotlin/com/example/dutamovie/DutamoviePlugin.kt
@@ -0,0 +1,13 @@
+package com.example.dutamovie
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class DutamoviePlugin : BasePlugin() {
+
+    override fun load(context: Context) {
+        registerMainAPI(Dutamovie())
+    }
+}
