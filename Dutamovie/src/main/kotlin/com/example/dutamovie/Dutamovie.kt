b13f04b2db097c98090f4d411e9cae06696776c2
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-08 11:03:42 +0700

    Update Dutamovie.kt

--- Dutamovie/src/main/kotlin/com/example/dutamovie/Dutamovie.kt
+++ Dutamovie/src/main/kotlin/com/example/dutamovie/Dutamovie.kt
@@ -8,7 +8,7 @@ import java.net.URI
 
 class Dutamovie : MainAPI() {
 
-    override var mainUrl = "https://offshorebankservices.com"
+    override var mainUrl = "https://204.3.234.75/"
     override var name = "Dutamovie"
     override var lang = "id"
 
