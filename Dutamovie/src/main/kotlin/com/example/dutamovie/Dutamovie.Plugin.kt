f8ee3765ca27d5c1839a3828c8d4a0c6953bc8c3
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-08 10:21:28 +0700

    Update Dutamovie.Plugin.kt

--- Dutamovie/src/main/kotlin/com/example/dutamovie/Dutamovie.Plugin.kt
+++ Dutamovie/src/main/kotlin/com/example/dutamovie/Dutamovie.Plugin.kt
@@ -4,9 +4,9 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
-class DutamoviePlugin : BasePlugin() {
-
-    override fun load(context: android.content.Context) {
+class DutamoviePlugin : Plugin() {
+    override fun load(context: Context) {
+        // Mendaftarkan class MainAPI Dutamovie agar terbaca di Cloudstream
         registerMainAPI(Dutamovie())
     }
 }
