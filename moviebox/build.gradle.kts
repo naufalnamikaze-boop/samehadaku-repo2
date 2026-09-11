44718157b3f037913a83c23e622e21e063956e5c
naufalnamikaze-boop <naufalnamikaze@gmail.com>
2026-09-11 13:58:44 +0700

    Update build.gradle.kts

--- moviebox/build.gradle.kts
+++ moviebox/build.gradle.kts
@@ -1,1 +1,36 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
 
+version = 2
+
+android {
+    namespace = "com.example.Dutamovie"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    val cloudstream by configurations
+    val implementation by configurations
+
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+
+    implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
+}
